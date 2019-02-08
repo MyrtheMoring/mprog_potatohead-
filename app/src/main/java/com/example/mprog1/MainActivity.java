7dfe6d559f4a03e3ca7bd3cbfbc40dc2d69db5bc
Myrthe Moring <myrthe.moring@student.uva.nl>
2019-02-08 10:31:55 +0200

    working program

--- app/src/main/java/com/example/mprog1/MainActivity.java
+++ app/src/main/java/com/example/mprog1/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.mprog1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] ids = {R.id.arms, R.id.hat, R.id.mustache, R.id.shoes, R.id.mouth, R.id.nose, R.id.glasses, R.id.eyes, R.id.eyebrows};
+        String[] ids_strings = {"arms", "hat", "mustache", "shoes", "mouth", "nose", "glasses", "eyes", "eyebrows"};
+
+        if (savedInstanceState != null){
+            for (int i = 0; i < 9; i++){
+                ImageView image = (ImageView) findViewById(ids[i]);
+                if (savedInstanceState.getInt(ids_strings[i]) ==View.VISIBLE){
+                    image.setVisibility(View.VISIBLE);
+                } else image.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        int[] ids = {R.id.arms, R.id.hat, R.id.mustache, R.id.shoes, R.id.mouth, R.id.nose, R.id.glasses, R.id.eyes, R.id.eyebrows};
+        String[] ids_strings = {"arms", "hat", "mustache", "shoes", "mouth", "nose", "glasses", "eyes", "eyebrows"};
+
+        for (int i = 0; i < 9; i++){
+            ImageView image = (ImageView) findViewById(ids[i]);
+            int vis = image.getVisibility();
+            outState.putInt(ids_strings[i], vis);
+        }
+    }
+
+    public void checkClicked(View v) {
+        Log.d("potato", "checkClicked");
+
+        CheckBox checkbox = (CheckBox) v;
+        boolean c = checkbox.isChecked();
+        String checkbox_text = checkbox.getText().toString();
+        int id = getResources().getIdentifier(checkbox_text, "id", getPackageName());
+        ImageView image = (ImageView) findViewById(id);
+
+        if (c) {
+            System.out.println(image);
+            image.setVisibility(View.VISIBLE);
+        } else image.setVisibility(View.INVISIBLE);
     }
 }
